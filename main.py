64a3c51b720f09f2e522454aecc2624e5cf8eea6
alimehridev <alimehridev@gmail.com>
2022-06-10 06:21:33 +0430

    oop has been added and app got more optimized

--- main.py
+++ main.py
@@ -1,87 +1,117 @@
-#!/usr/bin/python3 
-
-import tkinter
-from tkinter import ttk
+import tkinter as tk
+import threading
 import requests
 import json
 import subprocess
-import threading
 
+class App(tk.Frame):
+    def __init__(self, parent, *args, **kwargs):
+        tk.Frame.__init__(self, parent, *args, **kwargs)
+        self.parent = parent
+        
+        self.label = tk.Label(self, text="Word : ")
+        self.label.grid(row=0, column=0)
+        self.wordEnt = tk.Entry(self, width="50")
+        self.wordEnt.focus()
+        self.wordEnt.bind("<KeyRelease>", self.wordEntKeyRelease)
+        self.wordEnt.bind("<Down>", self.keyDownPressed)
+        self.wordEnt.bind("<Return>", self.keyEnterPressed)
+        self.wordEnt.grid(row=0, column=1)
 
-def words_list_selected(ev):
-    try:
-        selected_indicates = words_list.curselection()
-        selected = words_list.get(selected_indicates[0])
-        last_word = ent.get()
-        ent.delete(0, tkinter.END)
-        ent.insert(0, selected)
-    except:
-        pass
+        self.wordList = tk.Listbox(self, width="50")
+        self.wordList.bind("<Down>", self.wordListKeyPressed)
+        self.wordList.bind("<Up>", self.wordListKeyPressed)
+        self.wordList.bind("<Return>", self.wordListEnterKeyPressed)
 
-def one_less_word(ev):
-    value = ent.get().split(" ")
-    value = value[0:-1]
-    value = " ".join(value)
-    ent.delete(0, tkinter.END)
-    ent.insert(0, value)
+        # Get env file info
+        try:
+            envFile = open("/home/.config/simple-google-translator-python/env.txt", "r")
+            envFileContent = envFile.read().split("\n")
+            envFile.close()
+            envFileContentDict = {}
+            for info in envFileContent:
+                info = info.split("=")
+                envFileContentDict[info[0]] = info[1]
+            envFileContent = envFileContentDict
+            self.srcLang = envFileContent['SourceLanguage'] #source language
+            self.desLang = envFileContent['DestinationLanguage'] #destination language
+        except:
+            self.srcLang = "auto" #source language
+            self.desLang = "fa" #destination language
+            
+    def keyDownPressed(self, event):
+        self.wordList.focus()
+    def keyEnterPressed(self, event):
+        word = self.wordEnt.get()
+        if(word != ""):
+            self.translate(word)
+        
+    def wordListKeyPressed(self, event):
+        if (event.keysym == "Up"):
+            if(self.wordList.curselection()[0] == 0):
 
-def translate(ev):
-    word = ent.get()
-    if word == "":
-        return
-    
-    req = requests.get("https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=fa&hl=en-US&dt=t&dt=bd&dj=1&source=input&tk=29979.29979&q=" + word)
+                self.wordEnt.focus()
+        for i in self.wordList.curselection():
+            if(event.keysym == "Up"):
+                w = self.wordList.get(self.wordList.curselection()[0] - 1)
+            elif event.keysym == "Down":
+                w = self.wordList.get(self.wordList.curselection()[0] + 1)
+            self.wordEnt.delete(0, tk.END)
+            self.wordEnt.insert(0, w)
 
-    t = json.loads(req.text)
-    trans = t['sentences'][0]['trans']
-    try :
-        terms = t['dict'][0]['terms']
-        for i in terms:
-            trans += ", " + i
-    except:
-        pass
+    def wordListEnterKeyPressed(self, event):
+        if(len(self.wordList.curselection()) != 0):
+            word = self.wordList.get(self.wordList.curselection()[0])
+            self.wordEnt.delete(0, tk.END)
+            self.wordEnt.insert(0, word)
+            self.wordEnt.focus()
+            self.translate(self.wordEnt.get())
+        
+    def wordEntKeyRelease(self, event):
+        if (event.keysym == "Down"):
+            return
 
-    subprocess.Popen(['notify-send', trans])
-    root.destroy()
+        word = self.wordEnt.get()
+        if(word.strip() == ""):
+            self.wordList.grid_forget()
+            return
+        else:
+            autoCompleteThread = threading.Thread(target=self.auto_complete_req, args=(word, ))
+            autoCompleteThread.start()
 
+    def auto_complete_req(self, q):
+        req = requests.get("https://abadis.ir/ajaxcmd/getaclist/?exp=" + q)
+        content = req.text[1:].split("</span>")
+        content.pop()
 
-def auto_complete_req(q):
-    req = requests.get("https://api.datamuse.com/words?sp=" + q + "*")
-    values = json.loads(req.text)
-    values_list = []
-    for v in values:
-        values_list.append(v['word'])
-    words_list.config(listvariable=tkinter.StringVar(value=values_list))
-    words_list.pack(fill="x")
-    print(values_list)
+        for word in content:
+            index = content.index(word)
+            word = word.replace("<i class='ico icoSr'></i>", "")
+            word = word.replace("<span>", "")
+            content[index] = word
+        if (content == []):
+            self.wordList.grid_forget()
+            return
+        self.wordList.config(listvariable=tk.StringVar(value=content))
+        self.wordList.grid(row=1, column=1)
 
+    def translate(self, word):
+        req = requests.get("https://translate.googleapis.com/translate_a/single?client=gtx&sl=" + self.srcLang + "&tl=" + self.desLang + "&hl=en-US&dt=t&dt=bd&dj=1&source=input&tk=29979.29979&q=" + word)
 
-def callback(sv):
-    q = sv.get()
-    if(q != ""):
-        threading.Thread(target=auto_complete_req, args=(q, )).start()
-    else:
-        words_list.forget()
-root = tkinter.Tk()
-root.title("Google Translator")
+        t = json.loads(req.text)
+        trans = t['sentences'][0]['trans']
+        try :
+            terms = t['dict'][0]['terms']
+            for i in terms:
+                trans += ", " + i
+        except:
+            pass
 
-frame = ttk.Frame(root, padding="12 12 12 12")
-frame.pack(fill="x")
+        subprocess.Popen(['notify-send', trans])
+        self.parent.destroy()
 
-sv = tkinter.StringVar()
-sv.trace("w", lambda name, index, mode, sv=sv: callback(sv))
-
-
-entFrame = ttk.Frame(frame, padding="0 0 0 0")
-ent = tkinter.Entry(entFrame, width="50", textvariable=sv)
-ent.focus()
-ent.grid()
-entFrame.pack(fill="x")
-
-words_list = tkinter.Listbox(frame, height=10, selectmode='extended', fg="gray")
-words_list.bind("<<ListboxSelect>>", words_list_selected)
-
-root. bind('<Return>', translate)
-root. bind('<Control-BackSpace>', one_less_word)
-
-root.mainloop()
+if __name__ == '__main__':
+    root = tk.Tk()
+    root.title("Google Translator")
+    App(root).grid(padx="5", pady="5")
+    root.mainloop()
